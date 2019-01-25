2c4ca51a61e3abec933f95156f6f972676d3d95c
JessicaTatham <jectatham@gmail.com>
2019-01-25 11:46:53 -0800

    github battle

---
+++ app/components/Home.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='home-container'>
+        <h1> Battle your friends </h1>
+
+        <Link className='buttom' to='/battle'>
+          Battle
+        </Link>
+      </div>
+    )
+  }
+}
+
+module.exports = Home;
